adf3241dfaaaad6a805cc2874973c69dc661f9d0
aaizaguirre <aaizaguirreo@gmail.com>
2026-08-06 23:44:04 -0500

    feat: Implementa 'HEADER' en JSX y en 'App.css' y se agrega a 'App.jsx'

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './Header.jsx'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Header />
   )
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logoInmoPlus from '/public/logo-InmoPlus.PNG' 
+
+function Header(){
+
+    return(
+        <header>
+            <div id = "contenedor-logo">
+                <img id = "logo-img" src = {logoInmoPlus} alt = "Logo InmoPlus" />
+                <p id = "logo-titulo">INMOPLUS PERÚ</p>
+            </div>
+
+            <ul id = "navbar">
+                {/* TODO: Actualizar los 'href' luego de implementar las páginas */}
+                <li><a className = "active" href = "#">INICIO</a></li>
+                <li><a href = "#">COMPRAR</a></li>
+                <li><a href = "#">ALQUILAR</a></li>
+                <li><a href = "#">VENDER</a></li>
+                <li><a href = "#">NOSOTROS</a></li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
