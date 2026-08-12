49c31e24f600e6fb1933e74cceed0415b47c3296
aaizaguirre <aaizaguirreo@gmail.com>
2026-08-11 23:26:45 -0500

    fix: Se pasan la informaciónde propiedades hacia 'src/data'

---
+++ src/data/propiedades.jsx
@@ -0,0 +1,42 @@
+import depMiraflores1 from '../assets/dep-miraflores1.jpg'
+import depSanIsidro1 from '../assets/dep-san isidro1.jpg'
+import depBarranco1 from '../assets/dep-barranco1.jpg'
+import depSurco1 from '../assets/dep-surco1.jpg'
+
+export const propiedades = [ 
+    { id: 1, 
+        titulo: "Calle San Martín Cdra. 8 - Miraflores, Lima",
+        distrito: "Miraflores", 
+        estado: "En alquiler", 
+        precio: 5100, 
+        caracteristicas: "150 m2 3 dorm. 2 baños estacionamiento", 
+        imagen: depMiraflores1
+    }, 
+    { 
+        id: 2, 
+        titulo: "Av. Juan de Arona 110 - San Isidro, Lima", 
+        distrito: "San Isidro", 
+        estado: "En alquiler", 
+        precio: 2500, 
+        caracteristicas: "60 m2 2 dorm. 1 baño", 
+        imagen: depSanIsidro1 
+    },
+    { 
+        id: 3, 
+        titulo: "Jr. Sáenz Peña - Barranco, Lima", 
+        distrito: "Barranco", 
+        estado: "En alquiler", 
+        precio: 4000, 
+        caracteristicas: "110 m2 2 dorm. 2 baños 1 estacionamiento", 
+        imagen: depBarranco1 
+    }, 
+    { 
+        id: 4,
+        titulo: "Av. Circunvalación 148 - Surco, Lima",
+        distrito: "Surco", 
+        estado: "En alquiler", 
+        precio: 6000, 
+        caracteristicas: "120 m2 3 dorm. 2 baños 2 estacionamiento", 
+        imagen: depSurco1 
+    } 
+]; 
