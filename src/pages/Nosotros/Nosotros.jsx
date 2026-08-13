15608f12f82ab1c70bd5821cd1a9721c68c6bfde
aaizaguirre <aaizaguirreo@gmail.com>
2026-08-13 15:02:11 -0500

    feat: Agrega sección 'Testimonios' a 'Nosotros'

--- src/pages/Nosotros/Nosotros.jsx
+++ src/pages/Nosotros/Nosotros.jsx
@@ -1,6 +1,7 @@
 import Hero from '../../components/Hero';
 import SeccionNosotros from '../Home/SeccionNosotros';
 import SeccionMisionYVision from '../Nosotros/SeccionMisionYVision';
+import SeccionTestimonios from '../../components/SeccionTestimonios';
 
 import nosotros1 from '../../assets/equipo-inmobiliaria.jpg';
 import nosotros2 from '../../assets/equipo-inmobiliaria2.jpg';
@@ -24,6 +25,7 @@ function Nosotros(){
                 parrafo = "Somos una agencia inmobiliaria comprometida en brindar asesoría integral y personalizada. Nuestro equipo de profesionales cuenta con más de 20 años de experiencia guiando a familias e inversionistas a tomar la mejor decisión, garantizando seguridad jurídica, transparencia y acompañamiento desde la primera consulta hasta la entrega de llaves."
             />
             <SeccionMisionYVision/>
+            <SeccionTestimonios/>
         </>
     )
 }
