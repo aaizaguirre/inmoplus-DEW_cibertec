1832935e7ac3c41f3bbb098176f4d973795718a6
EdwinBellrick <e4dwine@gmail.com>
2026-08-14 18:13:24 -0500

    feat: implementa pagina comprar con filtros e imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,49 @@
 import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { getPropiedades, filtrarPropiedades } from '../src/services/propiedadesService.js';
+
+import { getPropiedades } from './services/propiedadesService.js';
+
 import './App.css';
 
-import Navbar from './components/Navbar.jsx'
-import Footer from './components/Footer.jsx'
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
 
-import Home from './pages/Home/Home.jsx'
+import Home from './pages/Home/Home.jsx';
+import Comprar from './pages/Comprar/Comprar.jsx';
 import Vender from './pages/vender/vender.jsx';
 import Confirmacion from './pages/vender/Confirmacion.jsx';
 import Nosotros from './pages/Nosotros/Nosotros.jsx';
+
 
 function App() { 
 
   const [todasLasPropiedades, setTodasLasPropiedades] = useState([]);
 
   useEffect(() => {
-    getPropiedades()
-      .then(setTodasLasPropiedades)
-      .catch((err) => console.error("Error al cargar propiedades:", err));
-  }, []);
+  getPropiedades()
+    .then(setTodasLasPropiedades)
+    .catch((err) => console.error('Error al cargar propiedades:', err));
+}, []);
   
   return (
       <>
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<Home todasLasPropiedades={todasLasPropiedades} />} />
-        <Route path="/vender" element={<Vender />} />
-        <Route path="/confirmacion" element={<Confirmacion />} />
-        <Route path = "/nosotros" element = {<Nosotros/>}/>
-      </Routes>
+  <Route
+    path="/"
+    element={<Home todasLasPropiedades={todasLasPropiedades} />}
+  />
+
+  <Route
+    path="/comprar"
+    element={<Comprar propiedades={todasLasPropiedades} />}
+  />
+
+  <Route path="/vender" element={<Vender />} />
+  <Route path="/confirmacion" element={<Confirmacion />} />
+  <Route path="/nosotros" element={<Nosotros />} />
+</Routes>
 
       <Footer />
     </>
